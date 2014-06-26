619827c4dc515424f97351626be2f6ccb15a7373
Kevin Bost <kevinbost@outlook.com>
2014-06-25 23:29:57 -0700

    Initial checkin, proof on concept with a single unit test appears to be working. Need a lot more unit tests.

---
+++ Binder.Core.Tests/ConditionalConverterTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Binder.Core.Tests
+{
+    [TestClass]
+    public class ConditionalConverterTests
+    {
+        [TestMethod]
+        public void SimpleInequalityComparision()
+        {
+            const string conditionFormat = "{0} > 3";
+            var converter = new ConditionalConverter();
+            object result = converter.Convert(new object[] {1}, null, conditionFormat, null);
+            Assert.AreEqual(false, result);
+        }
+    }
+}
---
+++ Binder.Core/ConditionalConverter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Windows;
+using System.Windows.Data;
+using DynamicExpression = System.Linq.Dynamic.DynamicExpression;
+using Expression = System.Linq.Expressions.Expression;
+
+
+namespace Binder.Core
+{
+    public class ConditionalConverter : IMultiValueConverter
+    {
+        private static readonly IDictionary<string, Func<object[], object>> _methods = new Dictionary<string, Func<object[], object>>();
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            string conditionFormat = parameter as string;
+            if (string.IsNullOrWhiteSpace(conditionFormat))
+                return DependencyProperty.UnsetValue;
+            return ConvertValues(conditionFormat, values);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new InvalidOperationException(string.Format("{0} may only be used in one way bindings", GetType().FullName));
+        }
+
+        private static object ConvertValues(string conditionFormat, object[] parameters)
+        {
+            lock (_methods)
+            {
+                Func<object[], object> method;
+                if (_methods.TryGetValue(conditionFormat, out method) == false)
+                {
+                    _methods[conditionFormat] = method = GenerateMethod(conditionFormat, parameters);
+                }
+                return method(parameters);
+            }
+        }
+
+        private static Func<object[], object> GenerateMethod(string conditionFormat, object[] parameters)
+        {
+            string formattedExpression = string.Format(conditionFormat,
+                Enumerable.Range(0, parameters.Length).Select(x => (object)string.Format("param{0}", x)).ToArray());
+            var funcParameters =
+                Enumerable.Range(0, parameters.Length)
+                    .Select(x => Expression.Parameter(parameters[x].GetType(), string.Format("param{0}", x)))
+                    .ToArray();
+
+            LambdaExpression expression = DynamicExpression.ParseLambda(funcParameters, typeof(object), formattedExpression);
+            Delegate compiledExpression = expression.Compile();
+            //TODO: This leaves much to be desired
+            Expression<Func<object[], object>> methodFunc = @params => compiledExpression.DynamicInvoke(@params);
+
+            return methodFunc.Compile();
+        }
+    }
+}
